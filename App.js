0939e5aadf80178ac5674891f08f49f9b1eb9131
Michal Mondik <m.mondik@gmail.com>
2018-05-09 16:46:14 +0200

    Configure eslint and refactor

--- App.js
+++ App.js
@@ -1,59 +1,45 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+// @flow
+import React from 'react';
 import {
-	Platform,
-	StyleSheet,
-	Text,
-	View
+    StyleSheet,
+    Text,
+    View,
 } from 'react-native';
 import ChartView from './ChartView';
 
-type Props = {};
-
-export default class App extends Component<Props> {
-	render() {
-		return (
-			<View style={styles.container}>
-				<View style={styles.navigation}>
-					<Text style={styles.navigationText}>Navigation Bar</Text>
-				</View>
-				<ChartView
-					xValues={["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]}
-					yValues={[20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]}
-					style={styles.map}
-				/>
-			</View>
-		);
-	}
-}
+const App = () => (
+    <View style={styles.container}>
+        <View style={styles.navigation}>
+            <Text style={styles.navigationText}>Navigation Bar</Text>
+        </View>
+        <ChartView
+            asd="asd"
+            xValues={['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']}
+            yValues={[20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]}
+        />
+    </View>
+);
 
 const styles = StyleSheet.create({
-	container: {
-		paddingTop: 20,
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F5FCFF',
-		alignItems: 'stretch',
-		flexDirection: 'column',
-		flex: 1,
-	},
-	map: {
-		flex: 1,
-	},
-	navigation: {
-		paddingLeft: 8,
-		paddingRight: 8,
-		backgroundColor: 'black',
-		height: 40,
-		justifyContent: 'center',
-	},
-	navigationText: {
-		color: 'white',
-	}
+    container: {
+        paddingTop: 20,
+        display: 'flex',
+        justifyContent: 'center',
+        backgroundColor: '#F5FCFF',
+        alignItems: 'stretch',
+        flexDirection: 'column',
+        flex: 1,
+    },
+    navigation: {
+        paddingLeft: 8,
+        paddingRight: 8,
+        backgroundColor: 'black',
+        height: 40,
+        justifyContent: 'center',
+    },
+    navigationText: {
+        color: 'white',
+    },
 });
+
+export default App;
--- ChartView.js
+++ ChartView.js
@@ -1,5 +1,33 @@
+// @flow
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import { requireNativeComponent } from 'react-native';
 
-const MODULE_NAME = 'Chart'; // it;s always looking for *Manager
+type Props = {|
+	+xValues: [String],
+	+yValues: [Int],
+|};
 
-export default requireNativeComponent(MODULE_NAME, null);;
+class ChartView extends Component<Props> {
+    render() {
+        const { xValues, yValues } = this.props;
+
+        return (
+            <Chart
+                xValues={xValues}
+                yValues={yValues}
+                style={{ flex: 1}}
+            />
+        );
+    }
+}
+
+ChartView.propTypes = {
+    xValues: PropTypes.array,
+    yValues: PropTypes.array,
+};
+
+const MODULE_NAME = 'Chart'; // it's always looking for *Manager
+const Chart = requireNativeComponent(MODULE_NAME, ChartView);
+
+export default ChartView;
